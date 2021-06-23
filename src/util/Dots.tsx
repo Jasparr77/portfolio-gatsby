1c476d4a37228b56b5b90f42ad202843f1d9529e
Jasper Croome <jasper@Jaspers-MacBook-Air.local>
2021-06-23 06:45:00 -0700

    moving curves

--- src/util/Dots.tsx
+++ src/util/Dots.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import * as d3 from 'd3';
+import { scaleLinear, line, curveCardinalClosed } from 'd3';
 
 const Dots:React.FC = () => {
   let i = 0;
-  const points:[number, number,number,number][] = [];
+  const points0:[number, number][] = [];
+  const points1:[number, number][] = [];
   while (i <= 42) {
-    const randomX2 = Math.random() * 100;
+    const randomX0 = Math.random() * 100;
+    const randomY0 = Math.random() * 100;
     const randomX1 = Math.random() * 100;
-    const randomY2 = Math.random() * 100;
     const randomY1 = Math.random() * 100;
-    points.push([randomX2, randomX1, randomY2, randomY1]);
+    points0.push([randomX0, randomY0]);
+    points1.push([randomX1, randomY1]);
     i += 1;
   }
 
@@ -17,60 +19,27 @@ const Dots:React.FC = () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
 
-  const xScale = d3.scaleLinear([0,100],[0,width]);
-  const yScale = d3.scaleLinear([0,100],[0,height]);
+  const xScale = scaleLinear([0,100],[0,width]);
+  const yScale = scaleLinear([0,100],[0,height]);
 
-  // const network = '';
+  const lineGenerator = line().x(d => xScale(d[0])).y(d => yScale(d[1])).curve(curveCardinalClosed)
+
   return (
     <svg overflow="visible">
-      {points.map((p,i)=>{
-        let index:number = (
-          i === 0 
-          ? points.length - 1
-          : i-1
-        )
-        return (
-          <>
-          <line 
-            stroke="black"
-            strokeWidth=".125px"
-            x1={xScale(p[0])}
-            y1={yScale(p[1])}
-            x2={xScale(points[index][0])}
-            y2={yScale(points[index][1])}
-          >
-            <animate 
-              attributeName="x1"
-              from={xScale(p[0])}
-              to={yScale(Math.random()*100)}
-              dur="30s"
-              repeatCount="indefinite"
-            />
-            <animate 
-              attributeName="y1"
-              from={yScale(p[1])}
-              to={xScale(Math.random()*100)}
-              dur="30s"
-              repeatCount="indefinite"
-            />
-            <animate 
-              attributeName="x2"
-              from={xScale(p[2])}
-              to={yScale(Math.random()*100)}
-              dur="30s"
-              repeatCount="indefinite"
-            />
-            <animate 
-              attributeName="y2"
-              from={yScale(points[index][1])}
-              to={xScale(Math.random()*100)}
-              dur="30s"
-              repeatCount="indefinite"
-            />
-            </line>
-            </>
-        )
-      })}
+      <path d={lineGenerator(points0)} strokeWidth=".5px"stroke="whiteSmoke" fill="salmon" opacity=".25">
+        <animate
+          attributeName="d" 
+          values={`${lineGenerator(points0)};${lineGenerator(points1)};${lineGenerator(points0)}`}
+          dur="300s"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="fill" 
+          values="salmon;slateBlue;lightgreen;slateBlue;salmon"
+          dur="300s"
+          repeatCount="indefinite"
+        />
+      </path>
       </svg>
   );
 };
